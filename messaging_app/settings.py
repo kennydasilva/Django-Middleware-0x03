ac5aa41575b6aa3ed31aa1a0c23abf9813b1b5ec
Kenny Dasilva <kennydasilva542@gmail.com>
2025-12-03 17:15:07 +0200

    addiction of the middlware

--- messaging_app/settings.py
+++ messaging_app/settings.py
@@ -74,6 +74,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'chats.middleware.RequestLoggingMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
